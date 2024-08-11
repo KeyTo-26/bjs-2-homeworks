23d58ad42808381c49d263ae74ec737e369eb797
Olga Chernyavtseva <kentra26@gmail.com>
2024-08-11 19:16:48 +0500

    Первое задание полностью готово

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,14 +1,14 @@
 'use strict'
 function solveEquation(a, b, c) {
-let arr = [];
-let d = b**2 - 4 * a * c;
+  let arr = [];
+  let d = b**2 - 4 * a * c;
 
-if (d < 0) {
-  arr = [];
-} else if (d === 0) {
-  arr = [-b / (2 * a)];
-} else if (d > 0) {
-  arr = [(-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a)];
+  if (d < 0) {
+    arr = [];
+  } else if (d === 0) {
+    arr = [-b / (2 * a)];
+  } else if (d > 0) {
+    arr = [(-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a)];
 }
 
 return arr;
